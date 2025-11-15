48f56ee9f6776604e05900ecdeb7ef1e9bbf6a54
Owen Buckley <owenbuckley13@gmail.com>
2025-11-15 13:32:48 -0500

    simple example

---
+++ .vercel/output/functions/products.func/index.js
@@ -0,0 +1,17 @@
+export default async function handler (request, response) {
+  const { body, url, headers = {}, method, query } = request;
+  console.log({ request });
+  console.log({ body, url, headers, method, query });
+  
+  response.status(200);
+  response.send(`
+    <html>
+      <head>
+        <title>Vercel Build Output API demo</title>
+      </head>
+      <body>
+        <h1>Hello from Products Page</h1>
+      </body>
+    </html>  
+  `);
+}
